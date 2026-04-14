5e3602695b4895d830f85333ca105d0386a0d74b
wkp48 <61016821+wkp48@users.noreply.github.com>
2026-04-14 12:04:40 +0900

    Title: 비교 연산, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260414/비교 연산/comparison-operator.py
@@ -0,0 +1,31 @@
+A,B = map(int, input().split())
+
+if A >= B:
+    print(f"1")
+else:
+    print(f"0")
+
+if A > B:
+    print(f"1")
+else:
+    print(f"0")
+
+if A <= B:
+    print(f"1")
+else:
+    print(f"0")
+
+if A < B:
+    print(f"1")
+else:
+    print(f"0")
+
+if A == B:
+    print(f"1")
+else:
+    print(f"0")
+
+if A != B:
+    print(f"1")
+else:
+    print(f"0")
