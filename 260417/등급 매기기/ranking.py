4b14956c401ded1bf2950bc488cccf3298b1d760
wkp48 <61016821+wkp48@users.noreply.github.com>
2026-04-17 14:13:21 +0900

    Title: 등급 매기기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260417/등급 매기기/ranking.py
@@ -0,0 +1,12 @@
+user_score = int(input())
+
+if user_score >= 90:
+    print("A")
+elif user_score >= 80:
+    print("B")
+elif user_score >= 70:
+    print("C")
+elif user_score >= 60:
+    print("D")
+else:
+    print("F")
