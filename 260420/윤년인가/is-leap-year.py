6431b80d5b20d4801c3b9b4c0b9add05b17cc069
wkp48 <61016821+wkp48@users.noreply.github.com>
2026-04-20 14:35:17 +0900

    Title: 윤년인가, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260420/윤년인가/is-leap-year.py
@@ -0,0 +1,12 @@
+year = int(input())
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(f"true")
+        else:
+            print(f"false")
+    else:
+        print(f"true")
+else:
+    print(f"false")
